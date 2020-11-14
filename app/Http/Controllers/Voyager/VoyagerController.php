a18fbd0a29ce64ec6916f37e9502d2c7b2b62c9d
Daniel <artexsdns@gmail.com>
2020-11-14 12:22:31 +0700

    latest roles

--- app/Http/Controllers/Voyager/VoyagerController.php
+++ app/Http/Controllers/Voyager/VoyagerController.php
@@ -11,30 +11,10 @@ class VoyagerController extends BaseVoyagerController
 
     public function index()
     {
-        // dd(Auth::user()->role_id);
-
         if(Auth::user()->role_id == 1 || Auth::user()->role_id == 2 || Auth::user()->role_id == 3 || Auth::user()->role_id == 4 || Auth::user()->role_id == 5 || Auth::user()->role_id == 6){ //admin pusat
+           
             return view('vendor.voyager.index');
 
-        }elseif(Auth::user()->role_id == 2){ //donatur
-            dd("donatur");
-        }elseif(Auth::user()->role_id == 3){ //petugas
-            dd("petugas");
-            
-        }elseif(Auth::user()->role_id == 4){ //admin cabang
-            dd("admin cabang");
-
-        }elseif(Auth::user()->role_id == 5){ //administrator
-            dd("administrator");
-
-        }elseif(Auth::user()->role_id == 6){ //normal user
-            // return Redirect::action('IssuerController@index');
-            dd("normal user");
-
-       
-        }else{
-    
-        //   return  view('voyager::XXXXXXX.cardholder');
         }
     }
 }
